3eebd8bb80123ba5c28ba7c1aeaf2461b6784d6e
Szymon Lechniak <szymonlechniak@gmail.com>
2023-05-12 12:14:46 +0200

    Added status codes

--- MillenniumAPI/Controllers/WeatherForecastController.cs
+++ MillenniumAPI/Controllers/WeatherForecastController.cs
@@ -22,32 +22,49 @@ namespace MillenniumAPI.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public ActionResult<IEnumerable<WeatherForecast>> Get()
         {
-            return _weatherForecastRepository.GetAllWeatherForecasts();
+            return Ok(_weatherForecastRepository.GetAllWeatherForecasts());
         }
 
         [HttpGet("{id}")]
-        public WeatherForecast Get(int id)
+        public ActionResult<WeatherForecast> Get(int id)
         {
-            return _weatherForecastRepository.GetWeatherForecast(id);
+            var item = _weatherForecastRepository.GetWeatherForecast(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return item;
         }
 
         [HttpPut]
-        public WeatherForecast Create(WeatherForecast weatherForecast)
+        public ActionResult<WeatherForecast> Create(WeatherForecast weatherForecast)
         {
-            return _weatherForecastRepository.Add(weatherForecast);
+            _weatherForecastRepository.Add(weatherForecast);
+            return CreatedAtAction(nameof(Get), new { id = weatherForecast.Id }, weatherForecast);
+            //return _weatherForecastRepository.Add(weatherForecast);
         }
 
         [HttpPatch]
         public WeatherForecast Update(WeatherForecast weatherForecastChanges)
         {
+            var item = _weatherForecastRepository.GetWeatherForecast(weatherForecastChanges.Id);
+            if(item == null)
+            {
+                NotFound();
+            }
             return _weatherForecastRepository.Update(weatherForecastChanges);
         }
 
         [HttpDelete("{id}")]
         public WeatherForecast Delete(int id)
         {
+            var item = _weatherForecastRepository.GetWeatherForecast(id);
+            if (item == null)
+            {
+                NotFound();
+            }
             return _weatherForecastRepository.Delete(id);
         }
     }
--- MillenniumAPI/Program.cs
+++ MillenniumAPI/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<IWeatherForecastRepository, MockWeatherForecastRepository>();
+builder.Services.AddControllers()
+    .AddXmlSerializerFormatters();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
