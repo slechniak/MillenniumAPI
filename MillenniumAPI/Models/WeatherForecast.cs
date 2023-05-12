118b273f173820145459031b7c205eae16e49349
Szymon Lechniak <szymonlechniak@gmail.com>
2023-05-12 12:37:41 +0200

    model validation

--- MillenniumAPI/Models/WeatherForecast.cs
+++ MillenniumAPI/Models/WeatherForecast.cs
@@ -1,14 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MillenniumAPI.Models
 {
     public class WeatherForecast
     {
         public int Id { get; set; }
+        
         public DateOnly Date { get; set; }
 
+        [Range(-90,60)]
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        [StringLength(20)]
         public string? Summary { get; set; }
     }
 }
