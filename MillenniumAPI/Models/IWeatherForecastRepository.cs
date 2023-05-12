4a98dbcea92a85f3aa649c53c7abd5f9b904f91a
Szymon Lechniak <szymonlechniak@gmail.com>
2023-05-12 10:56:42 +0200

    Add project files.

---
+++ MillenniumAPI/Models/IWeatherForecastRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MillenniumAPI.Models
+{
+    public interface IWeatherForecastRepository
+    {
+        WeatherForecast GetWeatherForecast(int Id);
+        IEnumerable<WeatherForecast> GetAllWeatherForecasts();
+        WeatherForecast Add(WeatherForecast weatherForecast);
+        WeatherForecast Update(WeatherForecast weatherForecastChanges);
+        WeatherForecast Delete(int Id);
+    }
+}
---
+++ MillenniumAPI/Models/MockWeatherForecastRepository.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MillenniumAPI.Models
+{
+    public class MockWeatherForecastRepository : IWeatherForecastRepository
+    {
+        private List<WeatherForecast> _weatherForecastList;
+
+        public MockWeatherForecastRepository()
+        {
+            string[] Summaries = new string[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            int id = 1;
+            _weatherForecastList = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Id = id++,
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
+
+        public WeatherForecast Add(WeatherForecast weatherForecast)
+        {
+            //throw new NotImplementedException();
+            weatherForecast.Id = _weatherForecastList.Max(e => e.Id) + 1;
+            _weatherForecastList.Add(weatherForecast);
+            return weatherForecast;
+        }
+
+        public WeatherForecast Delete(int Id)
+        {
+            //throw new NotImplementedException();
+            WeatherForecast weatherForecast = _weatherForecastList.FirstOrDefault(e => e.Id == Id);
+            if (weatherForecast != null)
+            {
+                _weatherForecastList.Remove(weatherForecast);
+            }
+            return weatherForecast;
+        }
+
+        public IEnumerable<WeatherForecast> GetAllWeatherForecasts()
+        {
+            //throw new NotImplementedException();
+            return _weatherForecastList;
+        }
+
+        public WeatherForecast GetWeatherForecast(int Id)
+        {
+            //throw new NotImplementedException();
+            return _weatherForecastList.FirstOrDefault(e => e.Id == Id);
+        }
+
+        public WeatherForecast Update(WeatherForecast weatherForecastChanges)
+        {
+            //throw new NotImplementedException();
+            WeatherForecast weatherForecast = _weatherForecastList.FirstOrDefault(e => e.Id == weatherForecastChanges.Id);
+            if (weatherForecast != null)
+            {
+                weatherForecast.Date = weatherForecastChanges.Date;
+                weatherForecast.TemperatureC = weatherForecastChanges.TemperatureC;
+                weatherForecast.Summary = weatherForecastChanges.Summary;
+            }
+            return weatherForecast;
+        }
+    }
+}
